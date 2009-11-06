1f7c5e23ccb7a9b99d9cff21d09a98b51b2f060e
Apkawa <apkawa@gmail.com>
2009-11-06 03:00:57 +0300

    cleanup files

--- uimge/hosts/imageshack_us.py
+++ uimge/hosts/imageshack_us.py
@@ -6,17 +6,20 @@ class Host(base.BaseHost):
 
     host='imageshack.us'
     action = 'http://imageshack.us/'
-    form = {'uploadtype': 'on', 'Submit':'"host it!"'}
-    def __init__(self):
-        self.ihost={\
-           'host':'imageshack.us', \
-           'post':'/', \
-           'cookie':''\
-           }
+    form = {
+            'uploadtype': 'on',
+            'Submit':'"host it!"'
+            }
+    max_file_size = 13145728 #13 Mb
+
     def as_file(self,_file):
         return {'fileupload': _file }
+#    def as_url(self,_file):
+#        action = 'http://www.imageshack.us/transload.php'
+#        return {'url': _file }
     def postload(self):
-        url=self.findall('value=\"(http://img.[\d]+?.imageshack.us/img[\d]+?/.*?/.*?)\"', self.response.body )
+        _src = self.response.body
+        url=self.findall('value=\"(http://img.[\d]+?.imageshack.us/img[\d]+?/.*?/.*?)\"', _src )
         tumburl=url[0].split('.')
         tumburl.insert(-1,'th')
         self.img_url = url[0]
